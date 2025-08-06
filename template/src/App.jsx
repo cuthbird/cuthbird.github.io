8cf9e6bcc708cefd5ea42ffd2ec06c2abc21890e
cuthbird <cuthberttan@gmail.com>
2025-08-06 16:37:23 +0800

    done

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import SearchBar from './components/SearchBar.jsx';
+import CityCard from './components/CityCard.jsx';
+import WeatherInfoCard from './components/WeatherInfoCard.jsx';
+import WeatherMap from './components/WeatherMap.jsx';
+import popularCities from './data/popularCities.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [selectedCity, setSelectedCity] = useState(popularCities[0]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen bg-gray-100 p-4">
+      {/* <SearchBar onSearch={setSelectedCity} /> */}
 
-export default App
+      <div className="my-4 overflow-x-auto whitespace-nowrap">
+        {popularCities.map((city) => (
+          <CityCard
+            key={city.id}
+            city={city}
+            lon={city.lon}
+            lat={city.lat}
+            onClick={() => setSelectedCity(city)}
+          />
+        ))}
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
+        <WeatherInfoCard city={selectedCity} />
+        <WeatherMap city={selectedCity} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
